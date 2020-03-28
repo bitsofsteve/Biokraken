bc1a796b1d3796cf0a82d9fd104d5cb5ffa90332
Cyberdroidmann <cyberlexix@gmail.com>
2020-03-28 15:42:17 +0100

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Biokraken',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This is a blog for biosciences, computer science, python for biologists, graphs, mathematics, coding',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
